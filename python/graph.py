d94f51280ac22251b97fe8017cc1f8d946b4e5fc
David Elliston <elliston@gmail.com>
2014-06-09 22:22:36 -0700

    Graph: Implemented breadth first search

--- python/graph.py
+++ python/graph.py
@@ -45,17 +45,6 @@ class Paths:
 		self.s = s #source
 		self.marked = [ False for _ in xrange(g.V()) ]	# FUTURE: array('B', [0 for ... ])
 		self.edgeTo = [ 0 for _ in xrange(g.V()) ]		# FUTURE: array('I', [0 for ... ])
-		self._gfs(self.s)
-
-	def _gfs(self, v):
-		print "Visit", v
-		self.marked[v] = True
-		for w in g.adj(v):
-			if not self.marked[w]:
-				self._gfs(w)
-				self.edgeTo[w] = v
-		print "Marked:", self.marked
-		print "EdgeTo:", self.edgeTo
 
 	def hasPathTo(self, v):
 		return self.marked[v]
@@ -73,15 +62,63 @@ class Paths:
 		# print "getPathTo(%d,%d) = %s" % (self.s, v, result)
 		return reversed(result)
 
+class DepthFirstPaths(Paths):
+	def __init__(self, g, s):
+		Paths.__init__(self, g, s)
+		self._dfs(self.s)		
+
+	def _dfs(self, v):
+		# print "Visit", v
+		self.marked[v] = True
+		for w in g.adj(v):
+			if not self.marked[w]:
+				self._dfs(w)
+				self.edgeTo[w] = v
+		# print "Marked:", self.marked
+		# print "EdgeTo:", self.edgeTo
+
+class BreadthFirstPaths(Paths):
+	def __init__(self, g, s):
+		Paths.__init__(self, g, s)
+		self._bfs()		
+
+	def _bfs(self):
+		from collections import deque
+		q = deque()
+		q.append(self.s)
+		self.marked[self.s] = True
+		while len(q):
+			v = q.popleft()
+			# print "Visit", v
+
+			for w in g.adj(v):
+				if not self.marked[w]:
+					q.append(w)
+					self.marked[w] = True
+					self.edgeTo[w] = v
+
+		# print "Marked:", self.marked
+		# print "EdgeTo:", self.edgeTo
+
+
 def disp(path):
-	if path is None: return "<None>"
-	else: return "[" + ",".join((str(x) for x in path)) + "]"
+	if path is None:
+		return "<None>"
+	else:
+		return "[%s]" % ",".join((str(x) for x in path)) 
 
 if __name__ == '__main__':
 	import sys
 	g = createGraph(sys.stdin)
 	printGraph(g)
 
-	paths = Paths(g, 0)
+	print "DepthFirst"
+	paths = DepthFirstPaths(g, 0)
+	for v in xrange(g.V()):
+		print "Path(%d,%d) = %s" % (0, v, disp(paths.getPathTo(v)))
+
+	print
+	print "BreadthFirst (shortest)"
+	paths = BreadthFirstPaths(g, 0)
 	for v in xrange(g.V()):
 		print "Path(%d,%d) = %s" % (0, v, disp(paths.getPathTo(v)))
